df3c57c1b20656012b8fd699d8fc0134bbd64a8c
Janelle Tavares <jtav77@gmail.com>
2013-06-17 01:44:58 -0700

    populate setup.rb file

--- lib/csv_reader.rb
+++ lib/csv_reader.rb
@@ -48,5 +48,20 @@ class CSVReader
   end
 
 
+  def read
+    f = File.new(@fname, 'r')
+
+
+    self.headers = f.readline
+
+
+    while (!f.eof? && next_line = f.readline)
+      values = next_line.split(',')
+      hash = create_hash(values)
+      yield(hash)
+    end
+  end
+
+
 end
 
--- lib/setup.rb
+++ lib/setup.rb
@@ -1,1 +1,26 @@
 # calls csv reader routine to create an Area instance for each row
+
+require_relative 'csv_reader'
+require_relative 'area'
+
+class Setup
+
+
+  attr_accessor :areas
+
+
+  def initialize
+    csv = CSVReader.new("./zipcode-db.csv")
+
+    @areas = []
+    csv.read do |item|
+      @areas << Area.new(item)
+    end
+
+    self
+
+
+  end
+
+
+end
